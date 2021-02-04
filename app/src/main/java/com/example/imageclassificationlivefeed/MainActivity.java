f51b665dfc224b4e399041efd892ab8ca5ce22de
hamza372 <31725222+hamza372@users.noreply.github.com>
2021-02-04 14:13:10 +0500

    Add files via upload

---
+++ app/src/main/java/com/example/imageclassificationlivefeed/MainActivity.java
@@ -0,0 +1,200 @@
+package com.example.imageclassificationlivefeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener{
+
+    Classifier classifier;
+    TextView resultTV;
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+
+        //TODO show live camera footage
+        setFragment();
+        resultTV = findViewById(R.id.textView);
+
+        //TODO inialize classifier class
+        try {
+            classifier = new Classifier(this,Classifier.Device.CPU,2,"mobilenet_v1_1.0_224.tflite","labels.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO fragment which show llive footage from camera
+    int previewHeight = 0,previewWidth = 0;
+    protected void setFragment() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+        try {
+            cameraId = manager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        Fragment fragment;
+        CameraConnectionFragment camera2Fragment =
+                CameraConnectionFragment.newInstance(
+                        new CameraConnectionFragment.ConnectionCallback() {
+                            @Override
+                            public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                previewHeight = size.getHeight();
+                                previewWidth = size.getWidth();
+                            }
+                        },
+                        this,
+                        R.layout.camera_fragment,
+                        new Size(640, 480));
+
+        camera2Fragment.setCamera(cameraId);
+        fragment = camera2Fragment;
+
+        getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+
+    //TODO getting frames of live camera footage and passing them to model
+    private boolean isProcessingFrame = false;
+    private byte[][] yuvBytes = new byte[3][];
+    private int[] rgbBytes = null;
+    private int yRowStride;
+    private Runnable postInferenceCallback;
+    private Runnable imageConverter;
+    private Bitmap rgbFrameBitmap;
+    @Override
+    public void onImageAvailable(ImageReader reader) {
+        // We need wait until we have some size from onPreviewSizeChosen
+        if (previewWidth == 0 || previewHeight == 0) {
+            return;
+        }
+        if (rgbBytes == null) {
+            rgbBytes = new int[previewWidth * previewHeight];
+        }
+        try {
+            final Image image = reader.acquireLatestImage();
+
+            if (image == null) {
+                return;
+            }
+
+            if (isProcessingFrame) {
+                image.close();
+                return;
+            }
+            isProcessingFrame = true;
+            final Image.Plane[] planes = image.getPlanes();
+            fillBytes(planes, yuvBytes);
+            yRowStride = planes[0].getRowStride();
+            final int uvRowStride = planes[1].getRowStride();
+            final int uvPixelStride = planes[1].getPixelStride();
+
+            imageConverter =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            ImageUtils.convertYUV420ToARGB8888(
+                                    yuvBytes[0],
+                                    yuvBytes[1],
+                                    yuvBytes[2],
+                                    previewWidth,
+                                    previewHeight,
+                                    yRowStride,
+                                    uvRowStride,
+                                    uvPixelStride,
+                                    rgbBytes);
+                        }
+                    };
+
+            postInferenceCallback =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            image.close();
+                            isProcessingFrame = false;
+                        }
+                    };
+
+            processImage();
+
+        } catch (final Exception e) {
+            Log.d("tryError",e.getMessage()+"abc ");
+            return;
+        }
+
+    }
+
+
+    String result = "";
+    private void processImage() {
+        imageConverter.run();
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
+        rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
+        List<Classifier.Recognition> recognitionList = classifier.recognizeImage(rgbFrameBitmap,getScreenOrientation());//classifier.recognizeImage(rgbFrameBitmap,getScreenOrientation());
+        result = "";
+        for(int i =0;i<recognitionList.size();i++){
+            Log.d("tryLog",recognitionList.get(i).getTitle());
+            result+=recognitionList.get(i).getTitle()+"  "+recognitionList.get(i).getConfidence()+"\n";
+        }
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                resultTV.setText(result);
+            }
+        });
+
+        postInferenceCallback.run();
+    }
+
+    protected void fillBytes(final Image.Plane[] planes, final byte[][] yuvBytes) {
+        // Because of the variable row stride it's not possible to know in
+        // advance the actual necessary dimensions of the yuv planes.
+        for (int i = 0; i < planes.length; ++i) {
+            final ByteBuffer buffer = planes[i].getBuffer();
+            if (yuvBytes[i] == null) {
+                yuvBytes[i] = new byte[buffer.capacity()];
+            }
+            buffer.get(yuvBytes[i]);
+        }
+    }
+    protected int getScreenOrientation() {
+        switch (getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_270:
+                return 270;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_90:
+                return 90;
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        classifier.close();
+    }
+}
